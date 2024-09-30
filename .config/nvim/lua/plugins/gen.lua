a99e6e5aaf96bc55ac965de34c4e01fb5f0d2181
Austin Kearns <59812315+Lillenne@users.noreply.github.com>
2024-09-30 10:58:00 -0700

    nvim genai

--- .config/nvim/lua/plugins/gen.lua
+++ .config/nvim/lua/plugins/gen.lua
@@ -2,7 +2,7 @@ return {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "llama3.1", -- The default model to use.
+      model = "codegemma:2b", -- The default model to use.
       quit_map = "q", -- set keymap for close the response window
       retry_map = "<c-r>", -- set keymap to re-send the current prompt
       accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
@@ -11,7 +11,7 @@ return {
       display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
       show_prompt = false, -- Shows the prompt submitted to Ollama.
       show_model = false, -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = true, -- Never closes the window automatically.
+      no_auto_close = false, -- Never closes the window automatically.
       hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
       init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
       -- Function to initialize Ollama
